5d9f8fc8365a1313b58d1cd8c7eb8552495aa9e9
shyntaayu <shynta.add@gmail.com>
2019-04-10 15:58:27 +0700

    + campur key

--- campur.php
+++ campur.php
@@ -221,7 +221,7 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=" . getenv('ACCOUNT_NAME') . ";AccountKey=" . getenv('ACCOUNT_KEY');
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=mimulwebapp;AccountKey=BNH5ljWlL1qfckqkYaPBC8De3uOThkSuvBB8hjwReNDuiVTyhxhxcN1ihYHZ71UumdLmiminbvhpr2uypSpSuQ==";
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
